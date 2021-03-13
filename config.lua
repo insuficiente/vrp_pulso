3723bb14fd932c9876f471bec4d225dbff18dd11
Eu sou <satuur.skid@gmail.com>
2021-03-12 22:38:52 -0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,22 +6,24 @@
  \_____\ |__|___|  /____  >____/ |__|  |__|\___  >__|\___  >___|  /__|  \___  >
                  \/     \/                     \/        \/     \/          \/ 
 
-[ SCRIPT PARA VER PULSO. SAMU/EMS ]
-[ PERMISSÃO DO SAMU/EMS DEVEM SER ALTERADAS AQUI!! ]
+[ PERMISSÕES PARA ADMIN E SAMU/EMS DEVEM SER ALTERADAS ABAIXO! ]
+[ SCRIPT PARA VER PULSO DO PLAYER DEITADO MAIS PRÓXIMO. FEITO PARA SAMU/EMS ]
 
-- Os scripts podem ser encontrados na nossa comunidade Discord ou GitHub. Links abaixo;
+- Os scripts podem ser encontrados na nossa comunidade pelo Discord ou GitHub. Links abaixo.
 - Não aceite scripts caros, você pode nos contatar para fazer um plugin por um preço acessível.
 
-Links para a nossa comunidade amigável! 🙂
+Links para a nossa comunidade! 🙂
 {
 	"GitHub": "https://github.com/insuficiente",
 	"Discord": "https://discord.gg/Exjftynpe9"	
 }
 
-Atualização: 08/03/2021 | v1.1.0
+Atualização: 12/03/2021 | v1.21.1
 
 Não tente revender este script/plugin, é totalmente feito pela nossa comunidade e não por você.
 Seria bem feio da sua parte vender um plugin gratuito, hein?
+
+DESEJAMOS UM ÓTIMO ROLEPLAY PARA A SUA CIDADE. ESPERAMOS QUE OS SEUS PLAYERS TENHAM A MELHOR EXPERIÊNCIA COM OS NOSSOS SCRIPTS.
 ]]
 local Pulso_CFG = {}
 
@@ -31,7 +33,32 @@ Pulso_CFG.Ems_Perm = "ems.permissao"
 -- Permissão do Admin
 Pulso_CFG.Adm_Perm = "administrador.permissao"
 
--- Tempo de respawn, será dividida em três partes(Ótimo estado, Estado de Alerta, Estado Grave)
+-- Tempo de respawn, será dividida em três partes(Ótimo estado, Estado de Alerta e Estado Grave)
 Pulso_CFG.Tempo_Respawn = 600 
+
+-- Configuração para a pulsagem do player, você pode alterar, porém já está configurado.
+Pulso_CFG.PulsoCfg = {
+	Pulso_CFG.Otimo_Estado = {
+		Pulso = {
+			MIN = 80,    -- Pulso Mínimo
+			MAX = 100,   -- Pulso Máximo
+			REVIVER = 90 -- Chance de reviver o player
+		}
+	},
+	Pulso_CFG.Estado_Alerta = {
+		Pulso = {
+			MIN = 50,    -- Pulso Mínimo
+			MAX = 70,    -- Pulso Máximo
+			REVIVER = 65 -- Chance de reviver o player
+		}
+	},
+	Pulso_CFG.Estado_Grave = {
+		Pulso = {
+			MIN = 30,    -- Pulso Mínimo
+			MAX = 45,    -- Pulso Máximo
+			REVIVER = 45 -- Chance de reviver o player
+		}
+	}
+}
 
 return Pulso_CFG
